b0ccbb6ca263f12132d4297bc1472d5c799dcffe
Ian Carrasco <iansee.c@gmail.com>
2019-04-30 11:31:12 -0700

    Added Scraper

---
+++ scraper.py
@@ -0,0 +1,75 @@
+#%%
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+import urllib
+import billboard
+from tqdm import tqdm
+import pandas as pd
+#%%
+def get_chord_url(song):
+    strRequest = song + " chords"
+
+    strRequest = urllib.parse.quote_plus(strRequest)
+
+    base_url = 'http://www.google.com/search?q=' + strRequest
+
+    search_results = requests.get(base_url)
+
+    soup = BeautifulSoup(search_results.text, "html.parser")
+
+    return soup.find('cite').text
+
+
+def get_chords(songs):
+    
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+
+    #Initialize Headless Selenium Instance
+    browser = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver', options=options)
+
+    dataframe = []
+
+    for idx, song in tqdm(enumerate(songs)):
+        try:
+            chord_url = get_chord_url(song[0].title)
+            if chord_url:
+                browser.get(chord_url)
+                html = browser.page_source
+                soup = BeautifulSoup(html)
+
+                chords = []
+                for match in soup.findAll('span', attrs={'class':'B24oE _1r_2U'}):
+                    chords.append(match.string)
+                
+                if len(chords) > 0:
+                    dataframe.append([song[0].artist, song[0].title, song[1], chords])
+
+            if idx % 50 == 0:
+                df = pd.DataFrame(dataframe, columns=["Artist", "Track", "Year", "Chords"])
+                df.to_csv('./chords.csv', index=False)
+        except Exception:
+            pass
+
+    return df
+#%%
+def process_songs(chart, year):
+    processed = []
+    for song in chart:
+        processed.append((song, year))
+    return processed
+
+songs = []
+for year in tqdm(range(1950, 2020)):
+    chart = billboard.ChartData('hot-100', date=str(year) + "-06-06")
+    songs += process_songs(chart, year)
+
+#%%
+
+df = get_chords(songs)
+
+#%%
+
+
+#%%
